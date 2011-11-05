3cf125c3c794471fd3182983d0752a988aac0884
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-11-05 10:39:59 +0100

    Add localizations that were contributed as comments on the plugin page.

---
+++ localization/jquery.prettydate-nl.js
@@ -0,0 +1,11 @@
+/* Dutch */
+$.prettyDate.messages = {
+	now: "nu net",
+	minute: "een minuut geleden",
+	minutes: $.prettyDate.template("{0} minuten geleden"),
+	hour: "1 uur geleden",
+	hours: $.prettyDate.template("{0} uur geleden"),
+	yesterday: "gisteren",
+	days: $.prettyDate.template("{0} dagen geleden"),
+	weeks: $.prettyDate.template("{0} weken geleden")
+}
