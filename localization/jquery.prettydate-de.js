7d89f1237edf476f88da081f5f9509d88d16be6c
ospalh <ospalh@gmail.com>
2012-06-25 09:48:09 +0200

    Adverb „gestern“, not noun „Gestern“

--- localization/jquery.prettydate-de.js
+++ localization/jquery.prettydate-de.js
@@ -5,7 +5,7 @@ $.prettyDate.messages = {
     minutes: $.prettyDate.template("vor {0} Minuten"),
     hour: "vor einer Stunde",
     hours: $.prettyDate.template("vor {0} Stunden"),
-    yesterday: "Gestern",
+    yesterday: "gestern",
     days: $.prettyDate.template("vor {0} Tagen"),
     weeks: $.prettyDate.template("vor {0} Wochen")
 };
