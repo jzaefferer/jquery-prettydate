aef559fea9a61bd5fafe4bdd8cbb2fc8dbb8232c
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-10-30 11:34:20 +0100

    Use tabs consistently

--- i18n/jquery.prettydate-de.js
+++ i18n/jquery.prettydate-de.js
@@ -1,16 +1,16 @@
 /* German */
 $.extend($.prettyDate.messages, {
-    now: 'gerade eben',
-    minute: 'vor einer Minute',
-    minutes: $.prettyDate.template('vor {0} Minuten'),
-    hour: 'vor einer Stunde',
-    hours: $.prettyDate.template('vor {0} Stunden'),
-    yesterday: 'gestern',
-    days: $.prettyDate.template('vor {0} Tagen'),
-    week: 'vor einer Woche',
-    weeks: $.prettyDate.template('vor {0} Wochen'),
-    month: 'vor einem Monat',
-    months: $.prettyDate.template('vor {0} Monaten'),
-    year: 'vor einem Jahr',
-    years: $.prettyDate.template('vor {0} Jahren')
+	now: 'gerade eben',
+	minute: 'vor einer Minute',
+	minutes: $.prettyDate.template('vor {0} Minuten'),
+	hour: 'vor einer Stunde',
+	hours: $.prettyDate.template('vor {0} Stunden'),
+	yesterday: 'gestern',
+	days: $.prettyDate.template('vor {0} Tagen'),
+	week: 'vor einer Woche',
+	weeks: $.prettyDate.template('vor {0} Wochen'),
+	month: 'vor einem Monat',
+	months: $.prettyDate.template('vor {0} Monaten'),
+	year: 'vor einem Jahr',
+	years: $.prettyDate.template('vor {0} Jahren')
 });
--- i18n/jquery.prettydate-es.js
+++ i18n/jquery.prettydate-es.js
@@ -1,16 +1,16 @@
 /* Spanish */
 $.extend($.prettyDate.messages, {
-    now: 'recién',
-    minute: 'hace un minuto',
-    minutes: $.prettyDate.template('hace {0} minutos'),
-    hour: 'hace una hora',
-    hours: $.prettyDate.template('hace {0} horas'),
-    yesterday: 'ayer',
-    days: $.prettyDate.template('hace {0} días'),
-    week: 'hace una semana',
-    weeks: $.prettyDate.template('hace {0} semanas'),
-    month: 'hace un més',
-    months: $.prettyDate.template('hace {0} meses'),
-    year: 'hace un año',
-    years: $.prettyDate.template('hace {0} años')
+	now: 'recién',
+	minute: 'hace un minuto',
+	minutes: $.prettyDate.template('hace {0} minutos'),
+	hour: 'hace una hora',
+	hours: $.prettyDate.template('hace {0} horas'),
+	yesterday: 'ayer',
+	days: $.prettyDate.template('hace {0} días'),
+	week: 'hace una semana',
+	weeks: $.prettyDate.template('hace {0} semanas'),
+	month: 'hace un més',
+	months: $.prettyDate.template('hace {0} meses'),
+	year: 'hace un año',
+	years: $.prettyDate.template('hace {0} años')
 });
