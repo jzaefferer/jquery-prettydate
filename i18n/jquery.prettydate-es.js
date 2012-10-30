892ab29e88774c2671c5a14ba5cedc3805a0c857
Fitorec <chanerec@gmail.com>
2012-10-30 11:32:50 +0100

    Extend spanish translation to include months and years

--- i18n/jquery.prettydate-es.js
+++ i18n/jquery.prettydate-es.js
@@ -1,11 +1,16 @@
 /* Spanish */
 $.extend($.prettyDate.messages, {
     now: 'recién',
-    minute: 'hace 1 minuto',
+    minute: 'hace un minuto',
     minutes: $.prettyDate.template('hace {0} minutos'),
-    hour: 'hace 1 hora',
+    hour: 'hace una hora',
     hours: $.prettyDate.template('hace {0} horas'),
     yesterday: 'ayer',
     days: $.prettyDate.template('hace {0} días'),
-    weeks: $.prettyDate.template('hace {0} semanas')
+    week: 'hace una semana',
+    weeks: $.prettyDate.template('hace {0} semanas'),
+    month: 'hace un més',
+    months: $.prettyDate.template('hace {0} meses'),
+    year: 'hace un año',
+    years: $.prettyDate.template('hace {0} años')
 });
