a5658bcd558ec58b96762e1fb90232a440cd417e
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-01-10 11:48:54 +0100

    i18n: Add czech translation

---
+++ i18n/jquery.prettydate-cs-CZ.js
@@ -0,0 +1,11 @@
+/* Czech */
+$.prettyDate.messages = {
+	now: 'před chvílí',
+	minute: 'před minutou',
+	minutes: $.prettyDate.template('před {0} minutami'),
+	hour: 'před hodinou',
+	hours: $.prettyDate.template('před {0} hodinami'),
+	yesterday: 'včera',
+	days: $.prettyDate.template('před {0} dny'),
+	weeks: $.prettyDate.template('před {0} týdny')
+};
