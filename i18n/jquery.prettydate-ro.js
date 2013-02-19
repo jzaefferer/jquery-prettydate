d9c00caa18fae64c1f8573b32911cba6648ad604
Radu <radu@wearex3.com>
2013-02-19 20:02:23 +0200

    Added Romanian translation. Closes gh-17

---
+++ i18n/jquery.prettydate-ro.js
@@ -0,0 +1,16 @@
+/* Romanian */
+$.extend($.prettyDate.messages, {
+	now: 'acum',
+	minute: 'acum un minut',
+	minutes: $.prettyDate.template('acum {0} minute'),
+	hour: 'acum o oră',
+	hours: $.prettyDate.template('acum {0} ore'),
+	yesterday: 'ieri',
+	days: $.prettyDate.template('acum {0} zile'),
+	week: 'acum o săptămână',
+	weeks: $.prettyDate.template('acum {0} săptămâni'),
+	month: 'acum o lună',
+	months: $.prettyDate.template('acum {0} luni'),
+	year: 'acum un an',
+	years: $.prettyDate.template('acum {0} ani')
+});
