af0d8b4d97d262b5b6087b400c537b24b700446c
Juwan Yoo <vhain6512@gmail.com>
2013-06-24 21:56:42 +0900

    Added Korean translation.

---
+++ i18n/jquery.prettydate-kr.js
@@ -0,0 +1,17 @@
+/* Korean */
+$.extend($.prettyDate.messages, {
+  now: '조금 전',
+  minute: '약 1분 전',
+  minutes: $.prettyDate.template('약 {0}분 전'),
+  hour: '약 1시간 전',
+  hours: $.prettyDate.template('약 {0}시간 전'),
+  yesterday: '어제',
+  dayBeforeYesterday: '그제',
+  days: $.prettyDate.template('{0}일 전'),
+  week: '지난주',
+  weeks: $.prettyDate.template('약 {0}주 전'),
+  month: '지난달',
+  months: $.prettyDate.template('약 {0}개월 전'),
+  year: '작년',
+  years: $.prettyDate.template('약 {0}년 전')
+});
