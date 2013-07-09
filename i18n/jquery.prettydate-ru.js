dd9dfe597ec8bb1d8dfb01ab9c156ed9074d627d
Oire <andre@oire.org>
2013-07-09 23:46:23 +0300

    Improve RU and UK localization, with support for singular forms ala '21 hour'. Closes gh-26

--- i18n/jquery.prettydate-ru.js
+++ i18n/jquery.prettydate-ru.js
@@ -1,30 +1,31 @@
 $.prettyDate.messages = (function() {
 
-	var FuncSource = function(few, many) {
+	var FuncSource = function(single, few, many) {
+		this.single = single;
 		this.few = few;
 		this.many = many;
 	};
 
 	FuncSource.prototype = {
 		replace: function(regexp, amt) {
-			return amt % 10 === 1 && amt % 100 !== 11 ? amt : (amt % 10 >= 2 && amt % 10 <= 4 && (amt % 100 < 10 || amt % 100 >= 20)? this.few.replace(regexp, amt): this.many.replace(regexp, amt));
+			return amt % 10 === 1 && amt % 100 !== 11 ? this.single.replace(regexp, amt) : (amt % 10 >= 2 && amt % 10 <= 4 && (amt % 100 < 10 || amt % 100 >= 20)? this.few.replace(regexp, amt): this.many.replace(regexp, amt));
 		}
 	};
 
 	return {
 		now: 'Только что',
-		minute: '1 минуту назад',
-		minutes: $.prettyDate.template(new FuncSource('{0} минуты назад', '{0} минут назад')),
-		hour: '1 час назад',
-		hours: $.prettyDate.template(new FuncSource('{0} часа назад', '{0} часов назад')),
+		minute: 'Минуту назад',
+		minutes: $.prettyDate.template(new FuncSource('{0} минуту назад', '{0} минуты назад', '{0} минут назад')),
+		hour: 'Час назад',
+		hours: $.prettyDate.template(new FuncSource('{0} час назад', '{0} часа назад', '{0} часов назад')),
 		yesterday: 'Вчера',
 		dayBeforeYesterday: 'Позавчера',
-		days: $.prettyDate.template(new FuncSource('{0} дня назад', '{0} дней назад')),
-		week: '1 неделю назад',
-		weeks: $.prettyDate.template(new FuncSource('{0} недели назад', '{0} недель назад')),
-		month: '1 месяц назад',
-		months: $.prettyDate.template(new FuncSource('{0} месяца назад', '{0} месяцев назад')),
-		year: '1 год назад',
-		years: $.prettyDate.template(new FuncSource('{0} года назад', '{0} лет назад'))
+		days: $.prettyDate.template(new FuncSource('{0} день назад', '{0} дня назад', '{0} дней назад')),
+		week: 'Неделю назад',
+		weeks: $.prettyDate.template(new FuncSource('{0} неделю назад', '{0} недели назад', '{0} недель назад')),
+		month: 'Месяц назад',
+		months: $.prettyDate.template(new FuncSource('{0} месяц назад', '{0} месяца назад', '{0} месяцев назад')),
+		year: 'Год назад',
+		years: $.prettyDate.template(new FuncSource('{0} год назад', '{0} года назад', '{0} лет назад'))
 	};
 })();
--- i18n/jquery.prettydate-uk.js
+++ i18n/jquery.prettydate-uk.js
@@ -6,31 +6,32 @@
 
 $.prettyDate.messages = (function() {
 
-	var FuncSource = function(few, many) {
+	var FuncSource = function(single, few, many) {
+		this.single = single;
 		this.few = few;
 		this.many = many;
 	};
 
 	FuncSource.prototype = {
 		replace: function(regexp, amt) {
-			return amt % 10 === 1 && amt % 100 !== 11 ? amt: (amt % 10 >= 2 && amt % 10 <= 4 && (amt % 100 < 10 || amt % 100 >= 20)? this.few.replace(regexp, amt): this.many.replace(regexp, amt));
+			return amt % 10 === 1 && amt % 100 !== 11 ? this.single.replace(regexp, amt): (amt % 10 >= 2 && amt % 10 <= 4 && (amt % 100 < 10 || amt % 100 >= 20)? this.few.replace(regexp, amt): this.many.replace(regexp, amt));
 		}
 	};
 
 	return {
 		now: 'Щойно',
-		minute: '1 хвилину тому',
-		minutes: $.prettyDate.template(new FuncSource('{0} хвилини тому', '{0} хвилин тому')),
-		hour: '1 годину тому',
-		hours: $.prettyDate.template(new FuncSource('{0} години тому', '{0} годин тому')),
+		minute: 'Хвилину тому',
+		minutes: $.prettyDate.template(new FuncSource('{0} хвилину тому', '{0} хвилини тому', '{0} хвилин тому')),
+		hour: 'Годину тому',
+		hours: $.prettyDate.template(new FuncSource('{0} годину тому', '{0} години тому', '{0} годин тому')),
 		yesterday: 'Вчора',
 		dayBeforeYesterday: 'Позавчора',
-		days: $.prettyDate.template(new FuncSource('{0} дні тому', '{0} днів тому')),
-		week: '1 тиждень тому',
-		weeks: $.prettyDate.template(new FuncSource('{0} тижні тому', '{0} тижнів тому')),
-		month: '1 місяць тому',
-		months: $.prettyDate.template(new FuncSource('{0} місяці тому', '{0} місяців тому')),
-		year: '1 рік тому',
-		years: $.prettyDate.template(new FuncSource('{0} роки тому', '{0} років тому'))
+		days: $.prettyDate.template(new FuncSource('{0} день тому', '{0} дні тому', '{0} днів тому')),
+		week: 'Тиждень тому',
+		weeks: $.prettyDate.template(new FuncSource('{0} тиждень тому', '{0} тижні тому', '{0} тижнів тому')),
+		month: 'Місяць тому',
+		months: $.prettyDate.template(new FuncSource('{0} місяць тому', '{0} місяці тому', '{0} місяців тому')),
+		year: 'Рік тому',
+		years: $.prettyDate.template(new FuncSource('{0} рік тому', '{0} роки тому', '{0} років тому'))
 	};
 })();
