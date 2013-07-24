e9a9aa83cd2c546d558be2f3f9473a218245ac93
Alberto <vandalo72@gmail.com>
2013-07-24 11:12:45 +0100

    Create jquery.prettydate-it.js
    Italian translation.

---
+++ i18n/jquery.prettydate-it.js
@@ -0,0 +1,17 @@
+/* Italian */
+$.extend($.prettyDate.messages, {
+  now: 'adesso',
+	minute: '1 minuto fa',
+	minutes: $.prettyDate.template('{0} minuti fa'),
+	hour: '1 ora fa',
+	hours: $.prettyDate.template('{0} ore fa'),
+	yesterday: 'ieri',
+	dayBeforeYesterday: 'l\'altro ieri',
+	days: $.prettyDate.template('{0} giorni fa'),
+	week: '1 settimana fa',
+	weeks: $.prettyDate.template('{0} settimane fa'),
+	month: 'un mese fa',
+	months: $.prettyDate.template('{0} mesi fa'),
+	year: 'un anno fa',
+	years: $.prettyDate.template('{0} anni fa')
+});
