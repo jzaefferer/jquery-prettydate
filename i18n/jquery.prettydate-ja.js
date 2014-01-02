581a1744430c00dd094b2a22b7f89abf968c1484
MH35 <mh35jp@gmail.com>
2014-01-02 13:07:11 +0900

    Japanese translation

---
+++ i18n/jquery.prettydate-ja.js
@@ -0,0 +1,17 @@
+/* Japanese */
+$.extend($.prettyDate.messages, {
+	now: 'たった今',
+	minute: '1分前',
+	minutes: $.prettyDate.template('{0}分前'),
+	hour: $.prettyDate.template('1時間前'),
+	hours: $.prettyDate.template('{0}時間前'),
+	yesterday: '1日前',
+	dayBeforeYesterday: '2日前',
+	days: '{0}日前',
+	week: '1週間前',
+	weeks: '{0}週間前',
+	month: '1ヶ月前',
+	months: '{0}ヶ月前',
+	year: '1年前',
+	years: $.prettyDate.template('{0}年前')
+});
