d729855d49ce7a3fed2ca7a7492139db41d5399a
yan <a0726h77@gmail.com>
2014-10-28 09:15:36 +0800

    Add Traditional Chinese locale

---
+++ i18n/jquery.prettydate-zh-TW.js
@@ -0,0 +1,17 @@
+/* Traditional Chinese */
+$.extend($.prettyDate.messages, {
+	now: '剛剛',
+	minute: '1分鐘前',
+	minutes: $.prettyDate.template('{0}分鐘前'),
+	hour: '1小時前',
+	hours: $.prettyDate.template('{0}小時前'),
+	yesterday: '昨天',
+	dayBeforeYesterday: '兩天前',
+	days: $.prettyDate.template('{0}天前'),
+	week: '1星期前',
+	weeks: $.prettyDate.template('{0}星期前'),
+	month: '1個月前',
+	months: $.prettyDate.template('{0}個月前'),
+	year: '1年前',
+	years: $.prettyDate.template('{0}年前')
+});
