0fa738b6359b460570fab5a781a47ade32c90e54
mh35 <mh35jp@gmail.com>
2015-06-27 13:18:46 +0900

    Translation bug fix and add demo

--- i18n/jquery.prettydate-ja.js
+++ i18n/jquery.prettydate-ja.js
@@ -7,11 +7,11 @@ $.extend($.prettyDate.messages, {
 	hours: $.prettyDate.template('{0}時間前'),
 	yesterday: '1日前',
 	dayBeforeYesterday: '2日前',
-	days: '{0}日前',
+	days: $.prettyDate.template('{0}日前'),
 	week: '1週間前',
-	weeks: '{0}週間前',
+	weeks: $.prettyDate.template('{0}週間前'),
 	month: '1ヶ月前',
-	months: '{0}ヶ月前',
+	months: $.prettyDate.template('{0}ヶ月前'),
 	year: '1年前',
 	years: $.prettyDate.template('{0}年前')
 });
